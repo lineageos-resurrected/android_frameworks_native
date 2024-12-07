0d5186fc9baa0f13f3eac3ef676945a9ec4b7c14
Ivy <78825510+Lost-Entrepreneur439@users.noreply.github.com>
2024-12-07 18:30:40 -0330

    Revert "Free mObjects if no objects left to realloc on resize"
    This reverts commit 163e091942b0f769765bc02052ac1d43cf9cf1c3.

--- libs/binder/Parcel.cpp
+++ libs/binder/Parcel.cpp
@@ -1647,16 +1647,10 @@ status_t Parcel::continueWrite(size_t desired)
                 }
                 release_object(proc, *flat, this);
             }
-
-            if (objectsSize == 0) {
-                free(mObjects);
-                mObjects = NULL;
-            } else { 
-                size_t* objects =
-                    (size_t*)realloc(mObjects, objectsSize*sizeof(size_t));
-                if (objects) {
-                    mObjects = objects;
-                }
+            size_t* objects =
+                (size_t*)realloc(mObjects, objectsSize*sizeof(size_t));
+            if (objects) {
+                mObjects = objects;
             }
             mObjectsSize = objectsSize;
             mNextObjectHint = 0;
